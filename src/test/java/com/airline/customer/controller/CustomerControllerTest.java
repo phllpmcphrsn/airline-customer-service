e6609287cdfd884ae3cb7cd8b11e98c4e7a87098
Phillip McPherson <pmcpherson@luf.co>
2023-03-03 21:45:20 -0500

    Add CustomerControllerTest

---
+++ src/test/java/com/airline/customer/controller/CustomerControllerTest.java
@@ -0,0 +1,25 @@
+package com.airline.customer.controller;
+
+import org.junit.jupiter.api.Test;
+
+public class CustomerControllerTest {
+    @Test
+    void testCreateCustomer() {
+
+    }
+
+    @Test
+    void testDeleteCustomer() {
+
+    }
+
+    @Test
+    void testGetCustomerById() {
+
+    }
+
+    @Test
+    void testUpdateCustomer() {
+
+    }
+}
